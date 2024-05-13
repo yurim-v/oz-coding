c8b3dab4fe5ce23c1620b2abe1eea65367d48a67
yurim-v <urdh96.w@gmail.com>
2024-05-13 21:36:40 +0900

    JS 4회차 project 혼자-CSS 2024.05.12~13

--- JavaScript/4회차_객체/project_Lotto_self.js
+++ JavaScript/4회차_객체/project_Lotto_self.js
@@ -1,4 +1,4 @@
-// 2024. 05. 12
+// 2024. 05. 12 ~ 2024.05.13
 
 
 // 날짜 설정
@@ -23,17 +23,37 @@ let ul = document.querySelector('ul');
 
 let num = []
 
-start.onclick = function(){
+let li ;
+
+function Create_num(){ // 랜덤 번호 생성 후 화면에 추가하는 함수
   while(num.length < 6){
     let ran = Math.floor(Math.random()* 45 + 1);
 
     if(num.indexOf(ran) === -1){
       num.push(ran);
-      let li =  document.createElement('li')
+      li =  document.createElement('li')
       li.textContent = ran;
       ul.append(li)
     }
-  
   }
-  
 }
+
+start.onclick = function(){
+
+  if(num.length < 6){
+    Create_num();
+  }
+  else{ // 추첨 후 다시 추첨 버튼을 눌렀을 때에도 랜덤 함수 생성
+    Reset_num()
+    Create_num()
+  }
+}
+
+  // 다시 버튼 눌렀을 때, 초기화 -> 배열 비워주고 -> 화면 지워주기
+
+    function Reset_num(){
+        num.splice(0,6);
+        ul.textContent =""
+    }
+
+    retry.addEventListener('click', Reset_num());
