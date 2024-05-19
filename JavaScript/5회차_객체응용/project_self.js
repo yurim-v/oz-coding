00333d613aeea7fe301231016a3f4d56a8654339
yurim-v <urdh96.w@gmail.com>
2024-05-19 19:13:45 +0900

    JS 5회차-To Do List 만들기-JS 2024.05.19

--- JavaScript/5회차_객체응용/project_self.js
+++ JavaScript/5회차_객체응용/project_self.js
@@ -1,38 +1,53 @@
-/* 2024. 05. 15 */
+/* 2024. 05. 15 , 05. 19 */
 /*
- 한계 -> 강의 보고 수정할 부분 + 아직 못한 내용 :
-  1. 클릭하는 대상인 p를 구분할 수 있도록 해야함 
-  -> p를 배열로 만들어서 idex로 구분하게 해야하나..?
-
-  2. map()으로 삭제되지 않은 말풍선 -> 배열로 새로 만들기
+ map()으로 삭제되지 않은 말풍선 -> 배열로 새로 만들기
   -> 로컬스토리지에다가 key는 myToDoList으로 value에는 할일 -> 객체 리터럴 -> JSON형식으로 저장 */
 
-
-// 추가 버튼 누르면 할일 말풍선 생성
-
+// 변수 선언
 let add = document.querySelector('form');
 let chat_content = document.getElementById('chat_content')
 
 let div ;
 let p ; 
 let rm_bt ;
+let p_arr = [];
+let Data = [];
+let i ;
+
+
+
+// ----------------------------------------------------------------------
 
 add.addEventListener('submit', function(event){
   event.preventDefault();
 
-  Create_msg(event)
+  // 할일 데이터 생성
+  let todo_data ={
+    todo : event.target.todo.value,
+    todoId : new Date().getTime(), // 객체 데이터 시간을 숫자로 반환 -> 식별자로 사용하기 위함
+    todoDone : false
+  }
+  Data.push(todo_data);
+  console.log(Data);
 
-  // 생성된 말풍선 클릭 시, 회색
-  p.setAttribute("onclick","Gray()");
+  localStorage.setItem('myToDo', JSON.stringify(p_arr));
 
-   // x 버튼 눌렀을 떄, 말풍선 삭제
-   Del_msg()
+  //  할일 말풍선 화면에 생성
+  Create_msg(event);
 
-  // localStorage.setItem()
+  event.target.todo.value= ""; // 할일 추가 후 텍스트 창 비우기 -> name으로 접근
 
+  // 말풍선 클릭 시, 말풍선 색깔 변함
+  Done()
+
+  // X 버튼 클릭 시, 말풍선 삭제
+  rm_bt.addEventListener("click", function(e){
+    e.target.parentNode.innerHTML="" // 부모요소 div 선택
+
+  })
 })
 
-//  할일 말풍선 생성
+//------------------------------ 할일 말풍선 화면에 생성
 function Create_msg(event){
   div = document.createElement('div')
   p = document.createElement('p')
@@ -48,28 +63,24 @@ function Create_msg(event){
   rm_bt.classList.add('remove')
   p.classList.add('message')
 
-  p.textContent = event.target.todo.value
+  p.textContent = event.target.todo.value;
   rm_bt.textContent = 'x'
+
+  p_arr.push(p)
 }
 
+// 클릭 시, 말풍선 색 변함 + 데이터 값 true 로 바뀜
+function Done(){
 
+  p.addEventListener('click', function(event){
+    // 말풍선 색깔 회색으로 바뀜
+    event.target.style.backgroundColor = 'lightgray';
 
-//------------- 말풍선 회색으로 변하는 Handler
-
-function Gray(){
-  p.classList.add('msg_rem');
- }
-
-
-// x 버튼 눌렀을 떄, 말풍선 삭제
-
-function Del_msg(){
-  rm_bt.addEventListener("click", function(){
-    div.innerHTML=""
+    // 데이터 값 변경
+  
   })
+
+
 }
-
-
-
 
 
